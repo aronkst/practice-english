94a44ef903722e05fcf43c4197de20cec595a922
Aron <aron.0504@hotmail.com>
2022-11-24 20:19:41 +0000

    created listen component

---
+++ src/lib/Listen.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let speech;
+	speech.onend = () => {
+		disableButtons = false;
+	};
+
+	let text = '';
+	let disableButtons = false;
+
+	function listenText() {
+		disableButtons = true;
+
+		speech.text = text;
+		window.speechSynthesis.speak(speech);
+	}
+</script>
+
+<div>
+	<button on:click={listenText} disabled={disableButtons}>Listen</button>
+</div>
+
+<textarea rows="10" cols="40" bind:value={text} />
