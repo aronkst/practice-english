98337e5a4c869ec804b67045d5641d74132e7427
Aron <aron.0504@hotmail.com>
2022-11-24 20:20:05 +0000

    created listenwrite component

---
+++ src/lib/ListenWrite.svelte
@@ -0,0 +1,65 @@
+<script>
+	import sentences from '../helper/sentences';
+	import stringSimilarity from 'string-similarity';
+
+	export let speech;
+	speech.onend = () => {
+		disableButtons = false;
+	};
+
+	let sentence = '';
+	let typed = '';
+	let similarity = -1;
+	let disableButtons = false;
+
+	function randomNumber() {
+		return Math.floor(Math.random() * sentences.length);
+	}
+
+	function randomSentence() {
+		let number = randomNumber();
+
+		while (sentence == sentences[number]) {
+			number = randomNumber();
+		}
+
+		sentence = sentences[number];
+		typed = '';
+		similarity = -1;
+	}
+
+	function listenSentence() {
+		disableButtons = true;
+
+		speech.text = sentence;
+		window.speechSynthesis.speak(speech);
+	}
+
+	function checkTyped() {
+		similarity = Math.ceil(
+			stringSimilarity.compareTwoStrings(sentence.toUpperCase(), typed.toUpperCase()) * 100
+		);
+	}
+
+	randomSentence();
+</script>
+
+<div>
+	<button on:click={randomSentence} disabled={disableButtons}>New Sentence</button>
+	<button on:click={listenSentence} disabled={disableButtons}>Listen</button>
+	<button on:click={checkTyped} disabled={disableButtons}>Check</button>
+</div>
+
+<textarea rows="10" cols="40" bind:value={typed} />
+
+{#if similarity >= 0}
+	<h2>{sentence}</h2>
+	<code>{similarity}%</code>
+{/if}
+
+<style>
+	code {
+		display: inline-block;
+		margin-top: 12px;
+	}
+</style>
