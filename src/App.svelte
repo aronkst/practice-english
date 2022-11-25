96b7dd811b3d3374fc9f82e1cc724b63f1a8167d
Aron <aron.0504@hotmail.com>
2022-11-25 00:50:32 +0000

    new design on index and app

--- src/App.svelte
+++ src/App.svelte
@@ -24,30 +24,34 @@
 	}
 </script>
 
-<button on:click={() => setShow(0)}>Pronunciation</button>
-<button on:click={() => setShow(1)}>Listen and Write</button>
-<button on:click={() => setShow(2)}>Listen</button>
-<button on:click={() => setShow(3)}>Speech</button>
-<button on:click={() => setShow(4)}>Listen and Speech</button>
+<div class="mt-5">
+	<div class="tabs tabs-boxed">
+		<button class="tab text-lg" class:tab-active={show === 0} on:click={() => setShow(0)}>Pronunciation</button>
+		<button class="tab text-lg" class:tab-active={show === 1} on:click={() => setShow(1)}>Listen and Write</button>
+		<button class="tab text-lg" class:tab-active={show === 2} on:click={() => setShow(2)}>Listen</button>
+		<button class="tab text-lg" class:tab-active={show === 3} on:click={() => setShow(3)}>Speech</button>
+		<button class="tab text-lg" class:tab-active={show === 4} on:click={() => setShow(4)}>Listen and Speech</button>
+	</div>
+</div>
 
-<hr />
-
-{#if show === 0}
-	<Pronunciation {speech} {recognition} />
-{/if}
-
-{#if show === 1}
-	<ListenWrite {speech} />
-{/if}
-
-{#if show === 2}
-	<Listen {speech} />
-{/if}
-
-{#if show === 3}
-	<Speech {recognition} />
-{/if}
-
-{#if show === 4}
-	<ListenSpeech {speech} {recognition} />
-{/if}
+<div class="p-2">
+	{#if show === 0}
+		<Pronunciation {speech} {recognition} />
+	{/if}
+	
+	{#if show === 1}
+		<ListenWrite {speech} />
+	{/if}
+	
+	{#if show === 2}
+		<Listen {speech} />
+	{/if}
+	
+	{#if show === 3}
+		<Speech {recognition} />
+	{/if}
+	
+	{#if show === 4}
+		<ListenSpeech {speech} {recognition} />
+	{/if}
+</div>
