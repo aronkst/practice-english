846b0b1435a6d9cc5a8c1e1250dd7f3bb004a31c
Aron <aron.0504@hotmail.com>
2022-11-27 14:35:50 +0000

    created text for about page

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-3xl mb-2 text-secondary font-bold">Practice English</h1>
+<h1 class="text-3xl mb-2 font-bold">Practice English</h1>
 <p class="text-base">
 	Practice English is an ad-free and analytics-free web application, and completely open source.
 </p>
@@ -6,25 +6,25 @@
 
 <h2 class="text-2xl mt-5 mb-2">Pronunciation</h2>
 <p class="text-base">
-	Todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo
+	Practice your pronunciation with random sentences.
 </p>
 
 <h2 class="text-2xl my-2">Listen and Write</h2>
 <p class="text-base">
-	Todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo
+	Practice your ear by listening to English phrases and typing what you understood.
 </p>
 
 <h2 class="text-2xl my-2">Listen</h2>
 <p class="text-base">
-	Todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo
+	Type some text in english and listen to its pronunciation.
 </p>
 
 <h2 class="text-2xl my-2">Speech</h2>
 <p class="text-base">
-	Todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo
+	Speak in English and see what you said in writing.
 </p>
 
 <h2 class="text-2xl my-2">Listen and Speech</h2>
 <p class="text-base">
-	Todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo todo
+	Write a text in English, listen to its pronunciation, and practice your pronunciation with this text.
 </p>
